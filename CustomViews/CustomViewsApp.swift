7d238c65797da48a8142f6cbefc5a84ba696eda3
iSangeSherpa <sangesherpa1215@gmail.com>
2024-01-22 10:22:23 +0545

    Initial Commit

---
+++ CustomViews/CustomViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewsApp.swift
+//  CustomViews
+//
+//  Created by Web and App on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
